6895161fe98c82e66bb91d32482e963e4a1ca72c
mi <529963732@qq.com>
2025-03-24 17:25:54 +0800

    6-1

--- src/services/cart.ts
+++ src/services/cart.ts
@@ -1,9 +1,30 @@
+import type { CartItem } from '@/types/cart'
 import { http } from '@/utils/http'
 
 export const CartAddAPI = async (data: { skuId: string; count: number }) => {
   return http({
     url: '/member/cart',
     method: 'POST',
+    data,
+  })
+}
+export const myCartAPI = async () => {
+  return http<CartItem[]>({
+    url: '/member/cart',
+    method: 'GET',
+  })
+}
+export const deleteCartAPI = (data: { ids: string[] }) => {
+  return http({
+    method: 'DELETE',
+    url: '/member/cart',
+    data,
+  })
+}
+export const changeCartAPI = (skuId: string, data: { selected?: boolean; count?: number }) => {
+  return http({
+    method: 'PUT',
+    url: `/member/cart/${skuId}`,
     data,
   })
 }
